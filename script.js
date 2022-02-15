fdcb131e721dc8c5c81d5d1236b350354356cc81
mbMosman <mbmosman@users.noreply.github.com>
2022-02-15 00:53:54 -0700

    added solution

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 let favoritesList = ['Raindrops on roses', 'Whiskers', 'Copper Kettles', 'Woolen mittens'];
 
-// TODO - Add code here
+// Select the ul element to add items to
+let ul = document.getElementById('favorite-things');
+
+// Add the favorites
+for (let favorite of favoritesList){
+  let newListItem = document.createElement('li');
+  newListItem.innerText = favorite;
+  ul.appendChild(newListItem);
+}
+
+// Remove the extra item
+let listItems = ul.children;
+let kettles = listItems[2];
+ul.removeChild(kettles);
+
+// Update an item
+let whiskers = listItems[1];
+whiskers.innerText = 'Kitten whiskers';
